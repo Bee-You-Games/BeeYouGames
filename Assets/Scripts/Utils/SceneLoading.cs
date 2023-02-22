b9e498440a6bbdb459c69622d904da1e0e3bb36f
Jeroen de Haan <info.jdehaan@gmail.com>
2023-02-22 15:31:35 +0100

    Implemented loading screens
    Loading screen will only work if the game is started from the persistent scene

--- Assets/Scripts/Utils/SceneLoading.cs
+++ Assets/Scripts/Utils/SceneLoading.cs
@@ -5,19 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoading : MonoBehaviour, ISceneLoading
 {
-    private Scene currentScene;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        currentScene = SceneManager.GetActiveScene();
+        if (LoadingScreenManager.Instance == null)
+            Debug.Log("No instance of LoadingScreenManager was found.\nWill load scenes with SceneManager instead.");
     }
 
-    public void LoadScene(string pSceneName) => SceneManager.LoadScene(pSceneName);
+    public void LoadScene(string pSceneName)
+    {
+        if (LoadingScreenManager.Instance != null)
+            LoadingScreenManager.Instance.LoadScene(pSceneName);
+        else
+            SceneManager.LoadScene(pSceneName);
+    }
 
-    public void LoadScene(int pSceneIndexNumber) => SceneManager.LoadScene(pSceneIndexNumber);
+    public void LoadScene(int pSceneIndexNumber)
+    {
+        if (LoadingScreenManager.Instance != null)
+            LoadingScreenManager.Instance.LoadScene(pSceneIndexNumber);
+        else
+            SceneManager.LoadScene(pSceneIndexNumber);
+    }
 
-    public void ReloadScene() => SceneManager.LoadScene(currentScene.name);
+    public void ReloadScene()
+    {
+        if (LoadingScreenManager.Instance != null)
+            LoadingScreenManager.Instance.LoadScene(SceneManager.GetActiveScene().name);
+        else
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
     public void ExitGame() => Application.Quit();
 }
