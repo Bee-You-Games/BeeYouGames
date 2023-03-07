42abd9cf19a0c99b241a5b52aa0182f65ac6a1cb
Artiom Matievschi <Artiom.Matievschi@gmail.com>
2023-03-07 14:16:52 +0100

    Update script

--- CICDscripts/buildAPK.bat
+++ CICDscripts/buildAPK.bat
@@ -1,12 +1,36 @@
 SET mypath=%~dp0
 
 set projectPath=%1
-set logFile=%2
-set outputPath=%3
-set buildTarget=%4
+set outputPath=%2
+set buildTarget=%3
 
-echo %mypath:~0,-1%/%logFile%
+call :joinpath %mypath% %projectPath%
+set projectPathFull=%Result%
+echo %projectPathFull%
 
-Unity -quit -batchmode -buildTarget Android -projectPath "%mypath:~0,-1%/%projectPath%" -logFile %mypath:~0,-1%/%logFile%  -executeMethod CICDUtils.Builder.BuildProject -outputPath %mypath:~0,-1%/%outputPath% -targetPlatform %buildTarget%
+call :joinpath %mypath% %logFile%
+set logPathFull=%Result%
+echo %logPathFull%
+
+call :joinpath %mypath% %outputPath%
+set outputPathFull=%Result%
+echo %outputPath%
+
+
+@REM call :joinpath mypath "..\Project.txt"
+@REM set projectPathFull=%Result%
+@REM echo %projectPathFull%
+
+
+@REM call:joinpath "C:\trunk\" "..\Project.txt"
+
+Unity -quit -batchmode -buildTarget Android -projectPath %projectPathFull% -logFile - -executeMethod CICDUtils.Builder.BuildProject -outputPath %outputPathFull% -targetPlatform %buildTarget%
+
+:joinpath
+set Path1=%~1
+set Path2=%~2
+if {%Path1:~-1,1%}=={\} (set Result=%Path1%%Path2%) else (set Result=%Path1%\%Path2%)
+goto :eof
+
 
 @REM Unity -quit -batchmode -buildTarget Android -projectPath "D:\Documents\Repos\BeeYouGames" -logFile "D:\Documents\Repos\BeeYouGames\CICDscripts\log.txt" -executeMethod CICDUtils.Builder.BuildProject -outputPath "D:\\Documents\\Repos\\BeeYouGames\\Builds\\MyGame3.apk" -targetPlatform "windows64"
