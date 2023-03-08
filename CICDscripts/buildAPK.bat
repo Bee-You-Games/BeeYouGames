9411884b45c33aae9b4c7f7ab501a28070ecf323
Artiom Matievschi <Artiom.Matievschi@gmail.com>
2023-03-08 14:26:44 +0100

    Sent via butler to itch io

--- CICDscripts/buildAPK.bat
+++ CICDscripts/buildAPK.bat
@@ -16,14 +16,6 @@ call :joinpath %mypath% %outputPath%
 set outputPathFull=%Result%
 echo %outputPath%
 
-
-@REM call :joinpath mypath "..\Project.txt"
-@REM set projectPathFull=%Result%
-@REM echo %projectPathFull%
-
-
-@REM call:joinpath "C:\trunk\" "..\Project.txt"
-
 Unity -quit -batchmode -buildTarget Android -projectPath %projectPathFull% -logFile "log.txt" -executeMethod CICDUtils.Builder.BuildProject -outputPath %outputPathFull% -targetPlatform %buildTarget%
 
 :joinpath
@@ -31,6 +23,3 @@ set Path1=%~1
 set Path2=%~2
 if {%Path1:~-1,1%}=={\} (set Result=%Path1%%Path2%) else (set Result=%Path1%\%Path2%)
 goto :eof
-
-
-@REM Unity -quit -batchmode -buildTarget Android -projectPath "D:\Documents\Repos\BeeYouGames" -logFile "D:\Documents\Repos\BeeYouGames\CICDscripts\log.txt" -executeMethod CICDUtils.Builder.BuildProject -outputPath "D:\\Documents\\Repos\\BeeYouGames\\Builds\\MyGame3.apk" -targetPlatform "windows64"
