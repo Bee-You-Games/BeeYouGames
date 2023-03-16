6fb72f041be758606e139a8bbfa15570384be987
silfaber <silfaber@gmail.com>
2023-03-16 18:43:03 +0100

    LeanTween import, Interaction UI polished
    Imported LeanTween for good game feel and visual touches. Used in the fade for the interaction UI. Interaction UI also appears above the interaction target's head instead of always appearing above the player's head.

--- Assets/Scripts/Gameplay/InteractionUI.cs
+++ Assets/Scripts/Gameplay/InteractionUI.cs
@@ -7,15 +7,23 @@ public class InteractionUI : MonoBehaviour
 {
     public bool IsDisplayed = false;
     private Camera mainCamera;
+    private Transform playerPosition;
+    private CanvasGroup panelGroup;
     [SerializeField] private GameObject uiPanel;
     [SerializeField] private TextMeshProUGUI promptText;
     [Range(0f, 10f)]
     [SerializeField] private float promptHeight;
+    [Range(0, 1.5f)]
+    [SerializeField] private float fadeTime = 0.5f;
     
     void Start()
     {
         mainCamera = Camera.main;
+        playerPosition = transform.parent.transform;
+        panelGroup = uiPanel.GetComponent<CanvasGroup>();
+        uiPanel.transform.parent = null;
         uiPanel.SetActive(false);
+
     }
 
     void LateUpdate()
@@ -25,22 +33,31 @@ public class InteractionUI : MonoBehaviour
 
     public void SetUp(string pPrompt, Vector3 pPosition)
     {
-        /*
-         * Code to put interaction UI above target head, currently not possible as interaction UI is child of player
+        
         Vector3 interactablePos = pPosition;
         interactablePos.y += promptHeight;
         transform.position = interactablePos;
-        */
+
         promptText.text = pPrompt;
+
         uiPanel.SetActive(true);
+        panelGroup.alpha = 0;
+        panelGroup.LeanAlpha(1, 0.5f);
         IsDisplayed = true;
     }
 
     public void Close()
     {
-        uiPanel.SetActive(false);
         IsDisplayed = false;
+        panelGroup.alpha = 1;
+        panelGroup.LeanAlpha(0, fadeTime);
+        StartCoroutine(DelayedDisable(uiPanel, fadeTime));
+    }
 
+    IEnumerator DelayedDisable(GameObject pObject, float pTime)
+    {
+        yield return new WaitForSeconds(pTime);
+        pObject.SetActive(false);
     }
 
     private void TurnToCamera()
