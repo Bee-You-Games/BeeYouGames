1d90889cdc7de385b7bcd5f8b53e554104c4f32b
silfaber <silfaber@gmail.com>
2023-03-28 15:37:56 +0200

    implemented temporary dance break for NPC
    put a timer on how long the NPC is unavailable after talking, along with switching the animator back to the default position.

--- Assets/Scripts/Gameplay/NPC.cs
+++ Assets/Scripts/Gameplay/NPC.cs
@@ -23,9 +23,7 @@ public class NPC : AEventAgent, IInteractable
 
     public bool Interact(PlayerInteractor interactor)
     {
-        animator.SetBool("Interacting", true);
-        Available = false;
-
+        StartCoroutine(DanceBreak());
         if ((actorRole == Role.Sender) || (actorRole == Role.Both && progressedState))
         {
             EventSend();
@@ -37,4 +35,13 @@ public class NPC : AEventAgent, IInteractable
 	{
 		
 	}
+
+    IEnumerator DanceBreak()
+    {
+        animator.SetBool("Interacting", true);
+        Available = false;
+        yield return new WaitForSeconds(3f);
+        animator.SetBool("Interacting", false);
+        Available = true;
+    }
 }
