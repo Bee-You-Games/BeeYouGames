30ad4c9a7ee839e1097c77bd7b769a4987ac9b37
silfaber <silfaber@gmail.com>
2023-03-29 17:15:02 +0200

    Companion movement
    basic movement towards player, staying on the same Y coordinate

--- Assets/Scripts/Gameplay/CompanionCreature.cs
+++ Assets/Scripts/Gameplay/CompanionCreature.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CompanionCreature : MonoBehaviour
 {
+    [SerializeField] private Transform playerObject;
+    [SerializeField] private float preferredDistance = 1;
+    private bool moving = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,21 @@ public class CompanionCreature : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Vector3.Distance(transform.position, playerObject.position) > preferredDistance && !moving)
+            StartCoroutine(MoveTowardsPlayer());
         
+    }
+
+    private IEnumerator MoveTowardsPlayer()
+    {
+        moving = true;
+        while (Vector3.Distance(transform.position, playerObject.position) > preferredDistance)
+        {
+            Vector3 targetPosition = new Vector3(playerObject.position.x, transform.position.y, playerObject.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, 7 * Time.deltaTime);
+            //transform.position -= (playerObject.position - transform.position) * 0.06f;
+            yield return null;
+        }
+        moving = false;
     }
 }
