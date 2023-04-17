03dd05a27edfc07c08352d80b5f679ddedab56c9
Jeroen de Haan <info.jdehaan@gmail.com>
2023-04-17 14:20:40 +0200

    Repeatable layer setup happens during playmode

--- Assets/Scripts/ParallaxEffect.cs
+++ Assets/Scripts/ParallaxEffect.cs
@@ -6,15 +6,18 @@ using UnityEngine;
 [ExecuteAlways]
 public class ParallaxEffect : MonoBehaviour
 {
-    //[HideInInspector]
+    [HideInInspector]
     public float Speed;
-    //[HideInInspector]
+    [HideInInspector]
     public bool isRepeating = true;
-    //[HideInInspector]
+    [HideInInspector]
     public bool isRepeatingRandom = false;
 
     private float startPosition;
     private float spriteLength;
+
+    private bool isInitialized = false;
+
     private CharacterController2D player;
     private Camera cam;
 
@@ -34,24 +37,22 @@ public class ParallaxEffect : MonoBehaviour
             if (player == null) Debug.LogError("Couldn't find a player object", this);
 
             startPosition = transform.position.x;
-            spriteLength = GetComponent<SpriteRenderer>().bounds.size.x;
+
+            spriteLength = GetComponent<SpriteRenderer>().bounds.size.x; if (isRepeating)
+            {
+                leftDuplicate = new GameObject($"{this.name} left");
+                leftDuplicate.transform.SetParent(this.transform);
+                var spriteRendererLeft = leftDuplicate.AddComponent<SpriteRenderer>();
+                spriteRendererLeft.sprite = GetComponent<SpriteRenderer>().sprite;
+                leftDuplicate.transform.position = transform.position - new Vector3(GetComponent<SpriteRenderer>().bounds.size.x, 0, 0);
+
+                rightDuplicate = new GameObject($"{this.name} right");
+                rightDuplicate.transform.SetParent(this.transform);
+                var spriteRendererRight = rightDuplicate.AddComponent<SpriteRenderer>();
+                spriteRendererRight.sprite = GetComponent<SpriteRenderer>().sprite;
+                rightDuplicate.transform.position = transform.position + new Vector3(GetComponent<SpriteRenderer>().bounds.size.x, 0, 0);
+            }
         }
-
-        if (isRepeating)
-        {
-            leftDuplicate = new GameObject($"{this.name} left");
-            leftDuplicate.transform.SetParent(this.transform);
-            var spriteRendererLeft = leftDuplicate.AddComponent<SpriteRenderer>();
-            spriteRendererLeft.sprite = GetComponent<SpriteRenderer>().sprite;
-            leftDuplicate.transform.position = transform.position - new Vector3(GetComponent<SpriteRenderer>().bounds.size.x, 0, 0);
-
-            rightDuplicate = new GameObject($"{this.name} right");
-            rightDuplicate.transform.SetParent(this.transform);
-            var spriteRendererRight = rightDuplicate.AddComponent<SpriteRenderer>();
-            spriteRendererRight.sprite = GetComponent<SpriteRenderer>().sprite;
-            rightDuplicate.transform.position = transform.position + new Vector3(GetComponent<SpriteRenderer>().bounds.size.x, 0, 0);
-        }
-
     }
 
     private void OnDestroy()
@@ -65,16 +66,16 @@ public class ParallaxEffect : MonoBehaviour
         //Check to see if unity is in playmode, because of the excecute always attribute
         if (Application.isPlaying)
         {
-            float temp = player.transform.position.x * (1 - Speed);
+            float tempPos = player.transform.position.x * (1 - Speed);
 
             float distance = player.transform.position.x * Speed;
             transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
 
             if (!isRepeating) return;
 
-            if (temp > startPosition + spriteLength)
+            if (tempPos > startPosition + spriteLength)
                 startPosition += spriteLength;
-            else if (temp < startPosition - spriteLength)
+            else if (tempPos < startPosition - spriteLength)
                 startPosition -= spriteLength;
         }
     }
