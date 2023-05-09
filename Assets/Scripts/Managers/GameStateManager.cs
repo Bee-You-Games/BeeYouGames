188771cffb8d2cb94c3b3f8e10074c3038e2d149
silfaber <silfaber@gmail.com>
2023-05-09 16:03:25 +0200

    New pause and game state system

---
+++ Assets/Scripts/Managers/GameStateManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    public static GameStateManager Instance;
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else
+			Debug.LogError("Multiple instances of GameStateManager have been found", this);
+	}
+
+	public GameState CurrentGameState { get; private set; }
+
+	public delegate void GameStateChangeHandler(GameState newGameState);
+	public event GameStateChangeHandler OnGameStateChanged;
+
+	public void SetState(GameState newGameState)
+	{
+		if (newGameState == CurrentGameState)
+			return;
+
+		CurrentGameState = newGameState;
+		OnGameStateChanged?.Invoke(newGameState);
+	}
+
+}
